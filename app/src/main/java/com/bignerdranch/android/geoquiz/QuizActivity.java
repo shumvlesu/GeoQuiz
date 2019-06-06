caf351cae2185f889468fc84bf57ca9955e4ee0d
shumvlesu <sashumihin@gmail.com>
2019-06-06 17:37:30 +0300

    81 страница

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.geoquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -28,10 +29,13 @@ public class QuizActivity extends AppCompatActivity {
     };
     private int mCurrentIndex = 0;
 
+    private static final String TAG = "QuizActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -95,8 +99,36 @@ public class QuizActivity extends AppCompatActivity {
         });
 
 
-
     updateQuestion();
+
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            Log.d(TAG, "onStart() called");
+        }
+        @Override
+        public void onResume() {
+            super.onResume();
+            Log.d(TAG, "onResume() called");
+        }
+        @Override
+        public void onPause() {
+            super.onPause();
+            Log.d(TAG, "onPause() called");
+        }
+        @Override
+        public void onStop() {
+            super.onStop();
+            Log.d(TAG, "onStop() called");
+        }
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            Log.d(TAG, "onDestroy() called");
+        }
+
+
 
     }
 
