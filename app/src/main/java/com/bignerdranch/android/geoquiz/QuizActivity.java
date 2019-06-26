ef42b973f53e116562847b17c41fa53acb62b30b
shumvlesu <sashumihin@gmail.com>
2019-06-26 11:54:20 +0300

    126 стр.

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -16,6 +16,8 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
 
+
+
     private ImageButton mNextButton;
     private ImageButton mPreviousButton;
     private Button mCheatButton;
@@ -128,7 +130,9 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Start CheatActivity
-                Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+                //Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
                 startActivity(intent);
             }
         });
